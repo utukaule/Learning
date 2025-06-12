67ca8fd4c8eb3f897bea8ee621f75013fb2a9140
Utkatsh Kaule <utkarshkaule7@gmail.com>
2025-06-12 23:23:59 +0530

    question done and little improvment is required.

---
+++ DSA/DSA Problems/Array Level 0/merge_sorted_arrays.js
@@ -0,0 +1,27 @@
+// leetcode question number 88 
+// Merge sorted array
+function merge_sort(nums1, m, nums2, n) {
+    let num1Copy = nums1.splice(0,m)
+    let p1 = 0;
+    let p2 = 0;
+
+    for(let i = 0 ; i < m+n ; i++){
+        if(num1Copy[p1] < nums2[p2]){
+            nums1[i] = num1Copy[p1]
+            p1++
+        }
+        else{
+            nums1[i] = nums2[p2]
+            p2++
+        }
+    }
+    return nums1;
+}
+
+let nums1 = [1, 2, 3];
+let nums2 = [2, 5, 6];
+let m = nums1.length ;
+let n = nums2.length;
+
+let result = merge_sort(nums1, m, nums2, n);
+console.log(result);
