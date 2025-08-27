66ad09d586fbc1a4c44b57dea7b4f2c517c17a03
Utkatsh Kaule <utkarshkaule7@gmail.com>
2025-08-28 01:23:28 +0530

    local storage start

---
+++ JavaScript/Js Learing/code with sheryians/Learning/timersAndIntervals_6/script.js
@@ -0,0 +1,18 @@
+// let timeInterval = setInterval(function(){
+//     let h1  = document.createElement("h1")
+//     h1.textContent= "hello"
+//     document.body.append(h1)
+    
+// },3000)
+// timeInterval()
+
+// let count = 10
+// let interval = setInterval(function(){
+//     if(count >= 0){
+//         console.log(count)
+//         count--;
+//     }
+//     else{
+//         clearInterval(interval)
+//     }
+// },1000)
