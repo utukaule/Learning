aeff67e8598f0ba96232270b233c8f28325bd6d4
Utkatsh Kaule <utkarshkaule7@gmail.com>
2025-08-30 13:09:51 +0530

    auto complete is completed main feature 1. calling api dummyjson 2. useState to store the result 3. show resultes if input is in focus 4. cache the input field in state call cache so while using back space the value will be cross checked with cache to reduce re-rendaring 5. using setTimeout and reture (unmount or in this case reset) this will reduce the api call to improve the performance

--- ReactJS/React Projects/autocomplete/src/App.js
+++ ReactJS/React Projects/autocomplete/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  // show result and setShowResults is used for focus and blur the input field
+  const [showResult, setShowResults] = useState(false);
+  // local caching
+  const [cache, setCache] = useState({});
+
+  const fetchDate = async () => {
+    if (cache[input]) {
+      console.log("Cache return", input);
+      setRecipes(cache[input]);
+      return;
+    }
+    console.log("api is call");
+    const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
+    const json = await data.json();
+    // console.log(json.recipes);
+    setRecipes(json?.recipes);
+    setCache((prev) => ({ ...prev, [input]: json?.recipes }));
+    // console.log("this is menu", recipes);
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      fetchDate();
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main">
+        <h1>AutoComplete</h1>
+        <div>
+          <input
+            type="text"
+            className="search-input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            // blur and focus on inupt field
+            onFocus={() => setShowResults(true)}
+            onBlur={() => setShowResults(false)}
+          />
+          {showResult && (
+            <div className={input.length == 0 ? "zero" : "result-container"}>
+              {recipes.map((r) => (
+                <span className="result" key={r.id}>
+                  {r.name}
+                </span>
+              ))}
+            </div>
+          )}
+        </div>
+        {/* recepices */}
+      </div>
     </div>
   );
 }
