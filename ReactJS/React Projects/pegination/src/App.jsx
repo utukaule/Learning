eb49b03ab1205597e3df58f4ee4c19c1c826f138
Utkatsh Kaule <utkarshkaule7@gmail.com>
2025-09-27 15:56:47 +0530

    done

--- ReactJS/React Projects/pegination/src/App.jsx
+++ ReactJS/React Projects/pegination/src/App.jsx
@@ -1,34 +1,58 @@
-import { useEffect } from "react";
-import axios from 'axios';
-import { useState } from "react";
-function App() {
-  const [products, setProducts] = useState([])
-  useEffect(()=>{
-    const handleData = async() =>{
-      const data = await axios('https://dummyjson.com/products')
-      
-      console.log("the products are:- ",data.data.products)
-      setProducts(data.data.products)
+import { useEffect, useState } from "react";
+import ProductCard from "./components/ProductCard";
 
-    }
-    handleData()
-  },[])
-  return (
+const PAGE_SIZE = 10;
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/products?limit=200");
+    const json = await data.json();
+    setProducts(json.products);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const totalProducts = products.length;
+  const noOfPages = Math.ceil(totalProducts / 10);
+  const start = currentPage * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+
+  const handlePageChange = (n) => {
+    setCurrentPage(n)
+  }
+  return !products.length ? (
+    "no products found"
+  ) : (
     <div>
-      <h1>We are learning pegination</h1>
-      <div className="main">
-        <div>
-        {
-          products.map((product)=>(<div key={product.id} className="product">
-              <img src={""} alt="" />
-            </div>)
-          )
-        }
+      <div>
+        <h1 className="heading">We lcome To Pegination</h1>
 
+        <div className="products-container">
+          {products.slice(start, end).map((p) => {
+            return (
+              <ProductCard key={p.id} image={p.thumbnail} title={p.title} />
+            );
+          })}
+        </div>
+        <div className="pagination-container">
+          <span onClick={()=>goToPreviousPage()}>◀️</span>
+          {[...Array(noOfPages).keys()].map((n) => (
+            <span className="page-number" key={n}
+                  onClick={()=>handlePageChange(n)}
+            >
+              {n}
+            </span>
+          ))}
+          <span onClick={()=>goToNextPage()}>▶️</span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ ReactJS/React Projects/pegination/src/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+const ProductCard = ({ image, title }) => {
+  return (
+    <div className="product-card">
+      <img src={image} alt={title} className="product-img" />
+      <span >{title}</span>
+    </div>
+  );
+};
+
+export default ProductCard;
